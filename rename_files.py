51e47462f277201efe6c89d8aa63374237ed946f
Luka Leko <luka.leko1@gmail.com>
2024-06-11 12:58:15 +0200

    remove clutter

--- rename_files.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-
-
-for i in range(15):
-    f_name = Path("problems", f"problem{i}.json")
-    new_name = Path("problems", f"problem-{i}.json")
-    if os.path.isfile(f_name):
-        os.rename(f_name, new_name)
